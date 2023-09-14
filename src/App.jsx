099c10ea4e83304372a4a59df3be5b2c7735167f
ManhAnNguyen <thungantn1999@gmail.com>
2023-09-14 21:50:18 +0700

    done hw

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,73 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./app.css";
 
-const circle = ["red", "yellow", "green"];
-const App = () => {
-  const [current, setCurrent] = useState(0);
+let intervalId;
 
-  const onClick = () => {
-    setCurrent(current === 3 ? 1 : current + 1);
-  };
+const arr = ["green", "yellow", "red"];
+//auto batching
+
+const App = () => {
+  const [time, setTime] = useState("");
+  const [start, setStart] = useState({
+    _start: false,
+    _index: 0,
+  });
+
+  useEffect(() => {
+    if (!start._start) return;
+    intervalId = setInterval(() => {
+      setTime(time - 1);
+      if (time <= 1) {
+        setStart({ _start: false, _index: 3 });
+        clearInterval(intervalId);
+        return;
+      }
+
+      if (time > 10) {
+        setStart({ ...start, _index: 1 });
+      } else {
+        setStart({ ...start, _index: 2 });
+      }
+    }, 100);
+
+    return () => clearInterval(intervalId);
+  }, [start._start, time]);
+
   return (
     <div>
-      {circle.map((item, index) => (
-        <div
-          className="circle"
-          key={index}
-          style={{
-            background: index + 1 === current ? item : "gray",
-          }}
-        ></div>
-      ))}
-      <input placeholder="Start" />
-      <input placeholder="Yellow" />
-      <button onClick={onClick}>Click</button>
+      <div>
+        {start._start && <h1>{time}</h1>}
+        <input
+          type="text"
+          value={time}
+          onChange={(e) => setTime(+e.target.value)}
+        />
+        <button onClick={() => setStart({ ...start, _start: true })}>
+          Start
+        </button>
+      </div>
+
+      <div className="light-container">
+        {arr.map((item, i) => (
+          <div
+            className="light"
+            key={item}
+            style={{
+              background: i + 1 === start._index ? item : "gray",
+            }}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default App;
+
+// import React from "react";
+
+// const App = () => {
+//   return <div>App</div>;
+// };
+
+// export default App;
