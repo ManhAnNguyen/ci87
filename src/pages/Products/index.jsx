7489cb0b335ecedbddef9294de3cdf6ffb1e57cd
ManhAnNguyen <thungantn1999@gmail.com>
2023-09-21 21:26:28 +0700

    add day 12

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,16 +1,42 @@
-import React from "react";
-import products from "../../data.json";
+import React, { useEffect, useState } from "react";
+// import products from "../../data.json";
 
 import "./styles.css";
 import Product from "../../components/Product";
+import axios from "axios";
+import BeatLoader from "react-spinners/BeatLoader";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsloading(true);
+      const response = await axios.get("https://fakestoreapi.com/products");
+
+      setProducts(response.data);
+      setIsloading(false);
+    };
+
+    getData();
+  }, []);
+
   return (
-    <div className="product-list">
-      {products.map((item, index) => {
-        return <Product key={index} item={item} />;
-      })}
-    </div>
+    <>
+      <button>get data</button>
+      {isLoading && (
+        <div className="isLoading">
+          {" "}
+          <BeatLoader />
+        </div>
+      )}
+      <div className="product-list">
+        {products.map((item, index) => {
+          return <Product key={index} item={item} />;
+        })}
+      </div>
+    </>
   );
 };
 
